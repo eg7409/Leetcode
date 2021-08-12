18076f269e316e0334e1319c179345602794d2f2
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-08-13 00:15:54 +0530

    Update 1302. Deepest Leaves Sum.cpp

--- 1302. Deepest Leaves Sum.cpp
+++ 1302. Deepest Leaves Sum.cpp
@@ -47,3 +47,60 @@ public:
         return ans;
     }
 };
+
+
+////////////////////////////////////////////////////////////////
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int deepestLeavesSum(TreeNode* root) {
+        int sum = 0,ans=0;
+        
+        queue<TreeNode*> q;
+        
+        q.push(root);
+        q.push(NULL);
+        
+        while(!q.empty()){
+            TreeNode* curr = q.front();
+            q.pop();
+            
+            if(curr == NULL && q.empty()){
+                ans = sum;
+                break;
+            }
+            else{
+                if(curr == NULL){
+                    ans = sum;
+                    sum=0;
+                    q.push(NULL);
+                }
+                else{
+                    sum += curr->val;
+                }
+            }
+            
+            if(curr != NULL && curr->left != NULL){
+                q.push(curr->left);
+            }
+            if(curr != NULL && curr->right!=NULL){
+                q.push(curr->right);
+            }
+            
+        }
+        return ans;
+    }
+};
