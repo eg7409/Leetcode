0e1016bdacf73abdb8899be9d187f0101980f389
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-09-24 20:23:26 +0530

    Create 2001. Number of Pairs of Interchangeable Rectangles.cpp

---
+++ 2001. Number of Pairs of Interchangeable Rectangles.cpp
@@ -0,0 +1,28 @@
+#define ll long long
+class Solution {
+public:
+    long long interchangeableRectangles(vector<vector<int>>& a) {
+        ll ans = 0;
+        map<double,int> m;
+        
+        int n = a.size();
+        vector<double> v;
+        
+        double t;
+        
+        for(int i=0;i<n;i++){
+            t = (double)a[i][0]/a[i][1];
+            v.push_back(t);
+        }
+        
+    
+        for(int i=0;i<n;i++){
+            if(m.find(v[i])!=m.end()){
+                ans += m[v[i]];
+            }
+            m[v[i]]++;
+        }
+        
+        return ans;
+    }
+};
