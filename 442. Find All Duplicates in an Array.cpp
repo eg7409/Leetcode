b2b3541da5be441047555bd3411c52cfd34792e4
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-10-08 03:23:01 +0530

    Create 442. Find All Duplicates in an Array.cpp

---
+++ 442. Find All Duplicates in an Array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& v) {
+        int n = v.size();
+        
+        vector<int> ans;
+        
+        for(int i=0;i<n;i++){
+            if(v[abs(v[i])-1]<0){
+                ans.push_back(abs(v[i]));
+            }
+            else{
+                v[abs(v[i])-1] =  -v[abs(v[i])-1];
+            }
+        }
+        return ans;
+    }
+};
