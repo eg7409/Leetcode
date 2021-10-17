5b7e182c5f39dea209198faf1ab3af9349d3cd2b
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-10-18 01:05:46 +0530

    New Code added

--- 695. Max Area of Island.cpp
+++ 695. Max Area of Island.cpp
@@ -58,3 +58,39 @@ public:
         return ans;
     }
 };
+
+
+///////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+class Solution {
+public:
+    
+    int solve(vector<vector<int> > v,vector<vector<bool> > &visited,int i,int j,int n,int m){
+        
+        if(i < 0 || i >= n || j < 0 || j >= m || visited[i][j] || v[i][j] == 0){
+            return 0;
+        }
+        
+        visited[i][j] = true;
+        
+        return (1 + solve(v,visited,i+1,j,n,m) + solve(v,visited,i-1,j,n,m) + solve(v,visited,i,j+1,n,m) +solve(v,visited,i,j-1,n,m));
+    }
+    int maxAreaOfIsland(vector<vector<int>>& v) {
+        
+        int n = v.size();
+        int m = v[0].size();
+        
+        int ans = 0;
+        
+        vector<vector<bool> > visited(n,vector<bool>(m,false));
+        
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(!visited[i][j] && v[i][j] == 1){
+                    ans = max(ans,solve(v,visited,i,j,n,m));
+                }
+            }
+        }
+        return ans;
+    }
+};
