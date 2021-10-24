5cf99e98a5d1c861f0d0041c46810d0f301781a0
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-10-24 22:46:58 +0530

    Create 45. Jump Game II.cpp

---
+++ 45. Jump Game II.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+   int jump(vector<int>& nums) {
+        int n = nums.size();
+        vector<int> ans(n,INT_MAX);
+        ans[0] = 0;
+        
+        int i=0, j=1;
+        while(i<nums.size()){
+            j=i+1;
+            while(j-i<=nums[i] && j<nums.size()){
+                ans[j] = min(ans[i]+1, ans[j]);
+                j++;
+            }
+            i++;
+        }
+        return ans[n-1];
+        
+    }
+};
