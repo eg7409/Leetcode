8dee825d498935ad1636bfe8887dff1648f7e9cb
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-11-17 15:30:20 +0530

    Commit

--- 213. House Robber II.cpp
+++ 213. House Robber II.cpp
@@ -30,3 +30,48 @@ public:
         return max(dp[n-2],max(dp[n-1],max(a[n],a[n-1])));
     }
 };
+
+/* Another Method*/
+
+class Solution {
+public:
+    int rob(vector<int>& v) {
+        
+        int n = v.size();
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        if(n==1){
+            return v[0];
+        }
+        if(n==2){
+            return max(v[0],v[1]);
+        }
+        
+        if(n==3){
+            return max(v[0],max(v[1],v[2]));
+        }
+        int without = v[0],with = v[1];
+        
+        for(int i=2;i<n-1;i++){
+            int temp = with;
+            
+            with = max(without + v[i], with);
+            without = max(without,temp);
+        }
+        
+        
+             int q1 = v[1] , q2 = v[2];
+            
+            for(int i=3;i<n;i++){
+                int t = q2;
+                q2 = max(q1+v[i],q2);
+                q1 = max(t,q1);
+            }
+        
+        
+        return max(max(with,without),max(q1,q2));
+    }
+};
