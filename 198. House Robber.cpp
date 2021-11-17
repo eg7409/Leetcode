510404386e05bc518240347f8efb64e5acbee433
Eishita Gupta <66865791+eg7409@users.noreply.github.com>
2021-11-17 13:52:47 +0530

    Create 198. House Robber.cpp

---
+++ 198. House Robber.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int rob(vector<int>& v) {
+        
+        int n = v.size();
+        
+        if(n == 0){
+            return 0;
+        }
+        
+        if(n==1){
+            return v[0];
+        }
+        
+        vector<int> dp(n+2);
+        
+        dp[0] = v[0];
+        dp[1] = v[1];
+        
+        for(int i=2;i<n;i++){
+            dp[i] = max(dp[i-2]+v[i],dp[i-1]);
+            dp[i-1] = max(dp[i-1],dp[i-2]);
+        }
+        
+        return max(dp[n-1],dp[n-2]);
+    }
+};
